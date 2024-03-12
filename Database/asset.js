cf343ab3ffa9d91d9c7c8068b2f8d32f93d1d6c1
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-03-12 20:05:55 +0530

    Version 1.0

--- Database/asset.js
+++ Database/asset.js
@@ -1,2 +1,5 @@
 let set2 = ["All","Free Api","Premium Api","Data support Api","Fast Api","Voluntiar's Api","Others"];
 let currentProduct=0;
+let pageList = ["HomePage","ApiPage","DocsPage","AboutPage","HelpPage"];
+let aboutList = ["abtn0","abtn1","abtn2"];
+let additionalList = ["SearchPage","AccountPage","OpenApiPage"];
