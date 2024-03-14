eb51b3188e5e8d065fb9026b3863f0c2537f727f
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-03-14 09:13:57 +0530

    Version 1.0

---
+++ Database/VerifiPage.js
@@ -0,0 +1,64 @@
+const verifiPage = {
+    htmlData: 
+    `
+        <h2>Verify Systems</h2>
+        <div class="security">
+            <div id="progress"></div>
+            <div id="task">
+                <div class="component">
+                    <div class="left_component">
+                        <div class="top_component">Browser</div>
+                        <div class="bottom_component" id="d1">loading..</div>
+                    </div>
+                    <div class="right_component" id="v1">&#x2b55;</div>
+                </div>
+                <div class="component">
+                    <div class="left_component">
+                        <div class="top_component">Version</div>
+                        <div class="bottom_component" id="d2">loading..</div>
+                    </div>
+                    <div class="right_component" id="v2">&#x2b55;</div>
+                </div>
+                <div class="component">
+                    <div class="left_component">
+                        <div class="top_component">Screen Ratio</div>
+                        <div class="bottom_component" id="d3">loading..</div>
+                    </div>
+                    <div class="right_component" id="v3">&#x2b55;</div>
+                </div>
+                <div class="component">
+                    <div class="left_component">
+                        <div class="top_component">Internet</div>
+                        <div class="bottom_component" id="d4">loading..</div>
+                    </div>
+                    <div class="right_component" id="v4">&#x2b55;</div>
+                </div>
+                <div class="component">
+                    <div class="left_component">
+                        <div class="top_component">Agent Name</div>
+                        <div class="bottom_component" id="d5">loading..</div>
+                    </div>
+                    <div class="right_component" id="v5">&#x2b55;</div>
+                </div>
+                <div class="component">
+                    <div class="left_component">
+                        <div class="top_component">Location</div>
+                        <div class="bottom_component" id="d6">loading..</div>
+                    </div>
+                    <div class="right_component" id="v6">&#x2b55;</div>
+                </div>
+                <div class="component">
+                    <div class="left_component">
+                        <div class="top_component">User Id</div>
+                        <div class="bottom_component" id="d7">loading..</div>
+                    </div>
+                    <div class="right_component" id="v7">&#x2b55;</div>
+                </div>
+            </div>
+        </div>
+        <div class="btnHolder">
+            <button class="btn">Cancel</button>
+            <button class="btn btn-primary"onclick="mainPage();">Next</button>
+        </div>
+    `
+}
