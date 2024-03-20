13d971b41d9c939db30f23d6eb5bcc8fb1505fc3
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-03-20 20:47:59 +0530

    Version 1.0

--- Database/asset.js
+++ Database/asset.js
@@ -3,3 +3,4 @@ let currentProduct=0;
 let pageList = ["HomePage","ApiPage","DocsPage","AboutPage","HelpPage"];
 let aboutList = ["abtn0","abtn1","abtn2"];
 let additionalList = ["SearchPage","AccountPage","OpenApiPage"];
+let jsonFilePath, userPortfolio;
