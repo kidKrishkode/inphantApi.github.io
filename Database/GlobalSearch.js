8a8fdbc5b300525bd5404eb60d6817e3a7d7218e
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-03-21 20:11:50 +0530

    Version 1.0

---
+++ Database/GlobalSearch.js
@@ -0,0 +1,9 @@
+const GlobalSearch = {
+    htmlData: `
+    <div id="closeSearch" onclick="gloSearchOff();">&times;</div>
+    <div id="userSearch"></div>
+    <div class="searchField" id="apiSearch"></div>
+    <div class="searchField" id="pageSearch"></div>
+    <div id="missSearch"><img src="./img/404.svg" alt="404"/></div>
+    `,
+}
